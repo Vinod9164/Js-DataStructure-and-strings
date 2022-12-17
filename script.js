bc8229d22a704ddadbfbd574fda0458c25efa875
Vinod <vinod8797@gmail.com>
2022-12-18 01:33:11 +0530

    18-12-2022/01:33

--- script.js
+++ script.js
@@ -78,6 +78,40 @@ const rest2 = {
 }
 
 
+//console.log('-----------Maps-------------);
+
+const rest = new Map();
+rest.set('name','Classico')
+rest.set(1,'Frenze ,Italy');
+// console.log(rest.set(2,'Lisbon Portugal'));
+
+rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+.set('open', 11)
+.set('close', 23)
+.set(true,'We are Opend D:')
+.set(false,'We are closed D:')
+console.log(rest.get('name'))
+console.log(rest.get(true))
+// console.log(rest.get('true'))//data types matter true is boolean
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+//map delete and contains
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+// rest.set([1,2],'Test'); doesnt Works because in Memory [1,2] and get7[1,2] are not same in memory
+//try to acess  Dom since it is also same
+rest.set(document.querySelector('h1'),'Heading');
+const arr1 = [1,2];
+console.log(rest.set(arr1,'Test'));
+console.log(rest);//Has and dlete updated map
+console.log(rest.size);
+
+
+
 // console.log('-----------------Looping Objects-----------------------') 
 
 
